af1e82ed922fc4544b1cd25e47a0f854d2826cec
danielortegac <deoc29@gmail.com>
2026-03-19 23:30:57 -0500

    Update data.js

--- diplomas/data.js
+++ diplomas/data.js
@@ -3,5 +3,10 @@ window.BASE_DE_DATOS_DIPLOMAS = {
     "nombre": "Ejemplo Alumno",
     "url": "/fotos/diplomas/ejemplo-diploma.jpg",
     "curso": "Diplomado IA"
+  },
+  "sol-acana": {
+    "nombre": "sol acana",
+    "url": "/fotos/diplomas/Sol%20Oca%C3%B1a.jpg",
+    "curso": "certifcado club"
   }
 };
