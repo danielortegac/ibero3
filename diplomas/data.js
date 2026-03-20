aac759a46fb2d98d7f76cc7814c6f856b4c7f458
danielortegac <deoc29@gmail.com>
2026-03-19 23:49:19 -0500

    Update data.js

--- diplomas/data.js
+++ diplomas/data.js
@@ -8,5 +8,15 @@ window.BASE_DE_DATOS_DIPLOMAS = {
     "nombre": "sol acana",
     "url": "/fotos/diplomas/Sol%20Oca%C3%B1a.jpg",
     "curso": "certifcado club"
+  },
+  "deoc-ortegas": {
+    "nombre": "deoc ortegas",
+    "url": "https://raw.githubusercontent.com/danielortegac/ohawelll/c7422a2e2e81b8d3f6ba2d058f25076694e0df27/ohawell/FAVICON%20OHA.jpeg",
+    "curso": "diplo"
+  },
+  "deoc-ortegass": {
+    "nombre": "deoc ortegass",
+    "url": "/fotos/diplomas/Sol%20Oca%C3%B1a.jpg",
+    "curso": "marketing"
   }
 };
