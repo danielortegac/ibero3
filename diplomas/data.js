f6a14eed3c04b34cfadb22c5902bdb07a7fd19de
danielortegac <deoc29@gmail.com>
2026-04-17 23:34:23 -0500

    Update data.js

--- diplomas/data.js
+++ diplomas/data.js
@@ -1,1 +1,7 @@
-window.BASE_DE_DATOS_DIPLOMAS = {};
+window.BASE_DE_DATOS_DIPLOMAS = {
+  "daniel-eduardo-ortega-corella": {
+    "nombre": "Daniel Eduardo Ortega Corella",
+    "url": "/fotos/diplomas/Sol%20Oca%C3%B1a.jpg",
+    "curso": "Marketing con IA"
+  }
+};
