6802cefedfcab5e6ca013380b8b8b38a8f57b559
danielortegac <deoc29@gmail.com>
2026-05-31 20:53:59 -0500

    C
    C

--- diplomas/data.js
+++ diplomas/data.js
@@ -1,7 +1,42 @@
 window.BASE_DE_DATOS_DIPLOMAS = {
-  "daniel-eduardo-ortega-corella": {
-    "nombre": "Daniel Eduardo Ortega Corella",
-    "url": "/fotos/diplomas/Sol%20Oca%C3%B1a.jpg",
-    "curso": "Marketing con IA"
+  "wilson-rodrigo-vega-ilaquiche": {
+    "nombre": "Wilson Rodrigo Vega Ilaquiche",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-wilson-rodrigo-vega-ilaquiche.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 12 horas"
+  },
+  "stalin-alejandro-parra-herrera": {
+    "nombre": "Stalin Alejandro Parra Herrera",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-stalin-alejandro-parra-herrera.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 12 horas"
+  },
+  "edwin-thomas-mosquera": {
+    "nombre": "Edwin Thomas Mosquera",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-edwin-thomas-mosquera.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 12 horas"
+  },
+  "yicela-arenas": {
+    "nombre": "Yicela Arenas",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-yicela-arenas.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 12 horas"
+  },
+  "paola-artunduaga-galindo": {
+    "nombre": "Paola Artunduaga Galindo",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-paola-artunduaga-galindo.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 12 horas"
+  },
+  "isabel-castillo-ramos": {
+    "nombre": "Isabel Castillo Ramos",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-isabel-castillo-ramos.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 12 horas"
+  },
+  "zuleika-del-alcazar-melendez": {
+    "nombre": "Zuleika Del Alcázar Meléndez",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-zuleika-del-alcazar-melendez.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 12 horas"
+  },
+  "maria-duque": {
+    "nombre": "Maria Duque",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-maria-duque.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 12 horas"
   }
 };
