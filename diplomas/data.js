39dd5765b1a081f336c3ef6bed7843596d5a089f
danielortegac <deoc29@gmail.com>
2026-08-19 21:32:15 -0500

    w
    w

--- diplomas/data.js
+++ diplomas/data.js
@@ -43,5 +43,120 @@ window.BASE_DE_DATOS_DIPLOMAS = {
     "nombre": "Saida Yarahuan",
     "url": "/fotos/diplomas/certificado-marketing-digital-ia-saida-yarahuan.png",
     "curso": "Certificado en Marketing Digital con Inteligencia Artificial · 12 horas"
+  },
+  "yarelis-lelis-chavez-contreras": {
+    "nombre": "Yarelis Lelis Chavez Contreras",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-yarelis-lelis-chavez-contreras.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "evelyn-orquera": {
+    "nombre": "Evelyn Orquera",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-evelyn-orquera.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "eskarlet-guadalupe-gutierrez-gutierrez": {
+    "nombre": "Eskarlet Guadalupe Gutiérrez Gutiérrez",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-eskarlet-guadalupe-gutierrez-gutierrez.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "juan-camilo-calvo-morales": {
+    "nombre": "Juan Camilo Calvo Morales",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-juan-camilo-calvo-morales.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "geovanny-guillermo-penafiel-tixi": {
+    "nombre": "Geovanny Guillermo Peñafiel Tixi",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-geovanny-guillermo-penafiel-tixi.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "yu-ju-chen": {
+    "nombre": "Yu Ju Chen",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-yu-ju-chen.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "olga-patricia-venegas-salcedo": {
+    "nombre": "Olga Patricia Venegas Salcedo",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-olga-patricia-venegas-salcedo.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "marco-alencastro": {
+    "nombre": "Marco Alencastro",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-marco-alencastro.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "edwin-baus-reyes": {
+    "nombre": "Edwin Baus Reyes",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-edwin-baus-reyes.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "andres-dario-posada-nino": {
+    "nombre": "Andrés Darío Posada Niño",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-andres-dario-posada-nino.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "july-elizabeth-zuleta-bastidas": {
+    "nombre": "July Elizabeth Zuleta Bastidas",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-july-elizabeth-zuleta-bastidas.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "carlos-fernando-sanchez-bravo": {
+    "nombre": "Carlos Fernando Sanchez Bravo",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-carlos-fernando-sanchez-bravo.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "cecilia-elizabeth-ventura-sauceda": {
+    "nombre": "Cecilia Elizabeth Ventura Sauceda",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-cecilia-elizabeth-ventura-sauceda.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "salma-cecilia-garcia-quintero": {
+    "nombre": "Salma Cecilia Garcia Quintero",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-salma-cecilia-garcia-quintero.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "edgar-alan-roldan-serralde": {
+    "nombre": "Edgar Alan Roldán Serralde",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-edgar-alan-roldan-serralde.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "veronica-martinez-martinez": {
+    "nombre": "Verónica Martínez Martínez",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-veronica-martinez-martinez.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "marcela-beltran": {
+    "nombre": "Marcela Beltran",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-marcela-beltran.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "dante-huanca": {
+    "nombre": "Dante Huanca",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-dante-huanca.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "jose-luis-sanchez": {
+    "nombre": "Jose Luis Sanchez",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-jose-luis-sanchez.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "jimmy-zapata-zapata": {
+    "nombre": "Jimmy Zapata Zapata",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-jimmy-zapata-zapata.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "olivia-bada": {
+    "nombre": "Olivia Bada",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-olivia-bada.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "sara-esquivel": {
+    "nombre": "Sara Esquivel",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-39h-sara-esquivel.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 39 horas · 19 de agosto de 2026"
+  },
+  "daniel-eduardo-ortega-corella": {
+    "nombre": "Daniel Eduardo Ortega Corella",
+    "url": "/fotos/diplomas/certificado-marketing-digital-ia-12h-daniel-eduardo-ortega-corella.png",
+    "curso": "Marketing Digital e Inteligencia Artificial · Certificado 12 horas · 30 de mayo del 2026"
   }
 };
